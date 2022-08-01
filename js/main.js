b121ac2f88b75835d3bfd36a58338b22e775c1c3
Youssef Tamer <youssef.tamer.jo.official@gmail.com>
2022-08-02 01:26:52 +0300

    Add Testimonials Section

--- js/main.js
+++ js/main.js
@@ -14,4 +14,38 @@ $(function () {
         $(this).innerHTML("hhh")
     });
 
+    function checkClients() {
+        // if ($('.client:first').hasClass('active')) {
+        //     $('.leftArrow').fadeOut(300);
+        // } else {
+        //     $('.leftArrow').fadeIn(300);
+        // }
+
+        // if ($('.client:last').hasClass('active')) {
+        //     $('.rightArrow').fadeOut(300);
+        // } else {
+        //     $('.rightArrow').fadeIn(300);
+        // }
+
+        $('.client:first').hasClass('active') ? $('.leftArrow').fadeOut(300) : $('.leftArrow').fadeIn(300)
+
+        $('.client:last').hasClass('active') ? $('.rightArrow').fadeOut(300) : $('.rightArrow').fadeIn(300)
+    };
+
+    checkClients()
+
+    $('.testimonialsSection i').click(function () {
+        if ($(this).hasClass('rightArrow')) {
+            $('.testimonialsSection .active').fadeOut(300, function () {
+                $(this).removeClass('active').next('.client').addClass('active').fadeIn()
+                checkClients()
+            })
+        } else {
+            $('.testimonialsSection .active').fadeOut(300, function () {
+                $(this).removeClass('active').prev('.client').addClass('active').fadeIn()
+                checkClients()
+            })
+        }
+    })
+
 });
