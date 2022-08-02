2f70c6dd2acc65f10d6d8e8a39c3567c88f923f3
Youssef Tamer <youssef.tamer.jo.official@gmail.com>
2022-08-02 19:27:13 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -48,4 +48,12 @@ $(function () {
         }
     })
 
+    var loader = document.querySelector(".loader");
+
+    window.addEventListener("load", vanish);
+
+    function vanish() {
+        loader.classList.add("disappear")
+    };
+
 });
